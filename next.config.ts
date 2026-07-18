f463d36d29b741c14efa1ded9a9ee0c2679926b5
dushyantsolanki <fullmoj70@gmail.com>
2026-07-18 16:26:59 +0530

    chore: update next.config.ts to allow all remote hostnames for images

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname: "**",
       },
       {
-        protocol: "https",
-        hostname: "img.freepik.com",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
+        protocol: "http",
+        hostname: "**",
       }
     ],
   },
